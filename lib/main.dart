e6252dfb0c66efadc4a5c84ac35dbc130b2ec7e7
IgmarLozadaBolivar <losadabolivar@gmail.com>
2026-06-14 11:30:05 -0500

    feat: implement UI with liquid_glass_widgets and configure ZonaPro font assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LiquidGlassWidgets.initialize();
+
+  runApp(LiquidGlassWidgets.wrap(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,41 +18,122 @@ class MyApp extends StatelessWidget {
       theme: CupertinoThemeData(
         brightness: Brightness.light,
         primaryColor: CupertinoColors.systemBlue,
+        textTheme: CupertinoTextThemeData(
+          textStyle: TextStyle(fontFamily: 'ZonaPro'),
+        ),
       ),
       home: HomeScreen(),
     );
   }
 }
 
+int _selectedIndex = 0;
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final isDarkMode = CupertinoTheme.of(context).brightness == Brightness.dark;
+
     // Recibe 2 argumentos { navigationBar (Barra superior) y Child (Cuerpo de la pantalla) }
-    return CupertinoPageScaffold(
+    return GlassScaffold(
       backgroundColor: CupertinoColors.systemGroupedBackground,
-      child: SafeArea(
+      body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
             children: [
+              // --- BARRA SUPERIOR ---
               Row(
                 children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(22.5),
-                    child: Image.asset(
-                      'assets/sandra.png',
+                    child: Image.network(
+                      'https://www.tapback.co/api/avatar.webp',
                       width: 45,
                       height: 45,
                       fit: BoxFit.cover,
                     ),
                   ),
+                  Spacer(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Hello, Sandra',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        'Today 25 Nov.',
+                        style: TextStyle(
+                          color: CupertinoColors.darkBackgroundGray,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  GlassButton(
+                    stretch: 0.2,
+                    width: 45,
+                    height: 45,
+                    icon: Icon(
+                      CupertinoIcons.search,
+                      size: 20,
+                      color: CupertinoColors.darkBackgroundGray,
+                    ),
+                    onTap: () {},
+                  ),
                 ],
               ),
+
+              // --- CUERPO ---
+              SizedBox(height: 20.0),
+
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     CupertinoListSection.insetGrouped(
+              //       margin: EdgeInsets.symmetric(
+              //         horizontal: 16.0,
+              //         vertical: 8.0,
+              //       ),
+              //       additionalDividerMargin: 0.0,
+              //       children: [
+              //         CupertinoListTile.notched(
+              //           title: Text('Emotional Balance'),
+              //           subtitle: Text('Emotional Balance'),
+              //           trailing: const CupertinoListTileChevron(),
+              //           onTap: () {
+              //             // Acción estática
+              //           },
+              //         ),
+              //       ],
+              //     ),
+              //   ],
+              // ),
             ],
           ),
         ),
+      ),
+
+      // --- BARRA DE NAVEGACIÓN ---
+      bottomBar: GlassBottomBar(
+        indicatorColor: CupertinoColors.systemGrey5,
+        barBorderRadius: 40,
+        barHeight: 80,
+        tabs: [
+          GlassBottomBarTab(icon: Icon(CupertinoIcons.home)),
+          GlassBottomBarTab(icon: Icon(CupertinoIcons.square_grid_2x2)),
+          GlassBottomBarTab(icon: Icon(CupertinoIcons.chart_bar_square)),
+          GlassBottomBarTab(icon: Icon(CupertinoIcons.person)),
+        ],
+        selectedIndex: _selectedIndex,
+        onTabSelected: (index) {},
       ),
     );
   }
